b3d3faff8956330bb5d589d6f3acfa0ec320cc58
boss-space <68944906+boss-space@users.noreply.github.com>
2020-11-26 21:12:55 +0300

    Айди

--- bot.py
+++ bot.py
@@ -45,7 +45,10 @@ async def on_ready ():
     await bot.change_presence (
         status = discord.Status.idle, 
         activity = game
+    Id = 781550030976450601
+    Channel().get_channel_by_id (id).send ('djjsjfj')
     )
+
 
 @add_command ('help') #Пример как делать комманды
 async def help (message):
