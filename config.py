34e81d379ffaeaaf247cb043594d3d5085e661a0
Misha Shlykov <60094849+Misha-python@users.noreply.github.com>
2020-11-27 11:08:28 +0300

    NADA

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ settings = {
     'id': 781404915142492171,
     'prefix': '!',
     'link': 'https://discord.com/oauth2/authorize?client_id=781404915142492171&scope=bot&permissions=8',
-    'server': 'https://discord.gg/WFX5ukJj2F'
+    'server': 'https://discord.gg/WFX5ukJj2F',
+    'channel': 781550030976450601
 }
