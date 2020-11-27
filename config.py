df0744f6282117e74063bea2a055b8e285cc9c3d
Misha Shlykov <60094849+Misha-python@users.noreply.github.com>
2020-11-27 09:03:30 +0300

    Так надо. Не менять

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ settings = {
     'token': 'NzgxNDA0OTE1MTQyNDkyMTcx.X79KCQ.vcdevmaaRYCADgMagz0iAJX7VTo',
     'bot': 'NuclearBot',
     'id': 781404915142492171,
-    'prefix': 'n!',
+    'prefix': '!',
     'link': 'https://discord.com/oauth2/authorize?client_id=781404915142492171&scope=bot&permissions=8',
     'server': 'https://discord.gg/WFX5ukJj2F'
 }
