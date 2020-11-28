3f8cd677e03611260a41c92c1595152a25cf52d9
Misha Shlykov <60094849+Misha-python@users.noreply.github.com>
2020-11-28 18:03:14 +0300

    Add files via upload

---
+++ db.py
@@ -0,0 +1,25 @@
+from firebase import Firebase
+
+configfb = {
+  "apiKey": "AIzaSyC3vGWkRWrBNLuz5YlysXZMZXGy0gT56LA",
+  "authDomain": "164893195950.firebaseapp.com",
+  "databaseURL": "https://avroraacha.firebaseio.com/",
+  "storageBucket": "avroraacha.appspot.com" 
+}
+
+firebase = Firebase (configfb)
+db = firebase.database ()
+
+def db_setchanse (chance, guild_id):
+    data = {"shans": chance}
+    db.child ("timeout").child (guild_id).set (data)
+
+def db_getchance (guild_id):
+    all_users = db.child ("timeout").get ()
+
+    for user in all_users.each ():
+        kkey = user.key ()
+        
+        if str (kkey) == str (guild_id):    
+            a = user.val ()
+            return a ["shans"]
