b6b72b8ff58687ef6599ce22a3f20cc9205dbea4
Misha Shlykov <60094849+Misha-python@users.noreply.github.com>
2020-11-28 18:03:29 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,25 +3,15 @@ from random import Random
 import discord
 from discord.ext import commands
 from requests import get
-from firebase import Firebase
 
 from config import settings
 from style import *
-
-configfb = {
-  "apiKey": "AIzaSyC3vGWkRWrBNLuz5YlysXZMZXGy0gT56LA",
-  "authDomain": "164893195950.firebaseapp.com",
-  "databaseURL": "https://avroraacha.firebaseio.com/",
-  "storageBucket": "avroraacha.appspot.com" 
-} # V config nada pihat
-
+from db import db_setchance, db_getchance
 
 ver = '0.2.1 FB4'
 commands_dict = {} 
 rand = Random ().random
 
-firebase = Firebase (configfb)
-db = firebase.database ()
 bot = commands.Bot (command_prefix = settings ['prefix'])
 
 def add_command (name): 
@@ -81,13 +71,12 @@ async def chat (message):
     await message.channel.send (txt)
     
 @add_command('setchance')
-async def setchance(message):
-    ch = str (all_digits (get_next (message, 'setchance')))
-    
-    data = {"shans": ch}
-    db.child("timeout").child(message.guild.id).set(data)
-    
-    await message.channel.send("шанс теперь равен"+ch+"%")
+async def set_chance(message):
+    chance = str (all_digits (get_next (message, 'setchance')))
+
+    db_setchance (chance, message.guild.id) 
+        
+    await message.channel.send (f'Shance now is {chance}%')
 
 @add_command ('clear')
 async def clear (message):
@@ -114,6 +103,7 @@ async def dbdb (message):
             a = user.val ()
             sss = a ["shans"]
             await message.channel.send (sss)
+            
 @bot.event
 async def on_guild_join (guild):
     data = {"shans": "20"}
@@ -156,20 +146,10 @@ async def on_message (message):
         if len (msg_part_ment) - 1:
             await message.channel.send (chat_bot (''.join (msg_part_ment), str (message.author.id)))
             return
-        idid = message.guild.id
-    
-    
-        all_users = db.child ("timeout").get ()
-        for user in all_users.each ():
-            kkey = user.key ()
-        
-        
-            if str (kkey) == str (idid):
-            
-                a = user.val ()
-                sss = a ["shans"]
+
+        chance = db_getchance (message.guild.id)
                                            
-        if (rand () * 100) < int(sss):
+        if (rand () * 100) < int(chance):
                                                
             await message.channel.send (chat_bot (message.content, str (message.author.id)))
 
