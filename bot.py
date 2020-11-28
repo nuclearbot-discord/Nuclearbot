adc06857708ededb50f47e0f6fbae1d2445a789e
boss-space <68944906+boss-space@users.noreply.github.com>
2020-11-28 15:47:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,7 +89,13 @@ async def clear (message):
 async def say (message):
     await message.delete ()
     await message.channel.send (get_next (message, 'say'))
-
+@add_command('dbdb')
+async def dbdb (message):
+  all_users = db.child("timeout").get()
+  for user in all_users.each():
+    if user.key()=="781409504435896320":
+      a=user.val()
+      message.channel.send(a["shans"])
 '''@bot.event
 async def on_guild_join (guild):
     data = {"shans": "20"}
