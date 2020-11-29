6ea10cfa18d61e920758e4a173e78dba41a09739
boss-space <68944906+boss-space@users.noreply.github.com>
2020-11-29 10:17:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ settings = {
     'link': 'https://discord.com/oauth2/authorize?client_id=781404915142492171&scope=bot&permissions=8',
     'server': 'https://discord.gg/WFX5ukJj2F',
     'channel': 781550030976450601
+    'channel2':781550092888571945
 }
