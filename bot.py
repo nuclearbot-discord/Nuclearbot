57de74cb0b769ee7aa7f44ec1585152a821cde39
boss-space <68944906+boss-space@users.noreply.github.com>
2020-11-29 16:30:38 +0300

    D

--- bot.py
+++ bot.py
@@ -75,7 +75,9 @@ async def chat (message):
     msg = get_next (message, 'chat')
     txt = chat_bot (msg, str (message.author.id))
     await message.channel.send (txt)
-    
+@add_command('addtobase')
+async def addtobase(message, toadd):
+    message.channel.send(toadd) 
 @add_command('setchance')
 async def set_chance(message):
     chance = str (all_digits (get_next (message, 'setchance')))
