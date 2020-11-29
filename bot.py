7ebe0b0cc6aef93d28700f5c970524bdd401f8fb
boss-space <68944906+boss-space@users.noreply.github.com>
2020-11-29 11:40:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -107,8 +107,13 @@ async def minecraft (message):
     author = message.author.id
 
     if True: # Todo: проверку пользователя
-        acccc=dbmcget()
-        await message.channel.send (acccc)
+        all_acc1 = db.child("accs").get()
+        accs2=[]
+        for user in all_acc1.each():
+            accs2.append(user.key()+":"+user.val())
+        rnd=random.choice(accs2)
+        accitog=rnd.split(":")
+        await message.channel.send( str(accitog[1])+"- пароль\n"+str(accitog[0])+"- почта")
     else:
         await message.channel.send ('No.')
 
