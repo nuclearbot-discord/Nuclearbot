49e261693c4e111de4d47228cd369b05b317090b
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-02 17:28:08 +0300

    Грамота

--- style.py
+++ style.py
@@ -2,7 +2,7 @@ from discord import Embed
 
 # TextID          = Val
 # Simple TXTs
-txt_status_before           = 'Versition: '
+txt_status_before           = 'версия: '
 txt_status_after            = ' | Working correctly'
 txt_no_command              = 'Uncorrect command'
 txt_help_not_command_before = 'To get help type `'
