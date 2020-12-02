1bba9ec4810d7fed68ee90d2139df9292c93fbec
Misha Shlykov <60094849+Misha-python@users.noreply.github.com>
2020-12-02 20:18:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 settings = {
-    'token': 'NzgxMTYyMjM1NjczOTY4NjUx.X75oBg.x_28YyV-PkdW337tdC7Nd30ts4s',
-    'bot': 'TestBot',
-    'id': 781162235673968651,
-    'prefix': '>',
-    'link': 'https://discord.com/oauth2/authorize?client_id=781162235673968651&scope=bot&permissions=8',
+    'bot': 'NuclearBot',
+    'id': 781404915142492171,
+    'prefix': '!',
+    'link': 'https://discord.com/oauth2/authorize?client_id=781404915142492171&scope=bot&permissions=8',
     'server': 'https://discord.gg/WFX5ukJj2F',
-    'channel': 782584270694776852
+    'channel': 782584270694776852,
+
+    'token': 'NzgxNDA0OTE1MTQyNDkyMTcx.X79KCQ.vcdevmaaRYCADgMagz0iAJX7VTo'
 }
