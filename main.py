501d4ad5f2f2e069c2d4755003881ae1df9a93cd
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-03 09:05:33 +0300

    Аоаоа

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ async def on_ready ():
     #activ_stream = discord.Streaming (name = txt_status.format (ver, str (len (bot.guilds))), url = 'https://m.twitch.tv/buster')
 
     actv_0 = discord.Streaming (name = 'NuclearBot support server' , url = 'https://m.twitch.tv/buster')
-    actv_1 = discord.Streaming (name = ver, url = 'https://m.twitch.tv/buster')
+    actv_1 = discord.Streaming (name = 'версия:'+ver, url = 'https://m.twitch.tv/buster')
     actv_2 = discord.Streaming (name = len (bot.guilds), url = 'https://m.twitch.tv/buster')
     
     while True:
