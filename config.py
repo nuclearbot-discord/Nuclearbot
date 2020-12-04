b89acc9b1a7df02224e68c2e7602ee69b26a1c4c
Misha Shlykov <60094849+Misha-python@users.noreply.github.com>
2020-12-04 20:33:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 settings = {
     'id': 781404915142492171,
     'prefix': '!',
-    'link': 'https://discord.com/oauth2/authorize?client_id=781404915142492171&scope=bot&permissions=8',
+    'link': 'https://discord.com/api/oauth2/authorize?client_id=781404915142492171&permissions=8&redirect_uri=https%3A%2F%2Fnuclearbot.ga%2F&scope=bot',
     'server': 'https://discord.gg/WFX5ukJj2F',
     'channel': 782584270694776852,
     'site': 'https://nuclearbot.ga/'
