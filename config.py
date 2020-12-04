6cebdab8d7c9e2715a5a4f2c7fc32c066030ba8b
Misha Shlykov <60094849+Misha-python@users.noreply.github.com>
2020-12-04 20:30:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ settings = {
     'link': 'https://discord.com/oauth2/authorize?client_id=781404915142492171&scope=bot&permissions=8',
     'server': 'https://discord.gg/WFX5ukJj2F',
     'channel': 782584270694776852,
-
+    'site': 'https://nuclearbot.ga/'
     'token': 'NzgxNDA0OTE1MTQyNDkyMTcx.X79KCQ.vcdevmaaRYCADgMagz0iAJX7VTo'
 }
