055aa6d77657837fd522c05e1e5d72f08c8366f0
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-07 19:17:38 +0300

    U

--- db.py
+++ db.py
@@ -59,7 +59,7 @@ Def addadm(idid):
 def addcoint(id, coins):
     ctex=usrgetc(id) 
     data = {
-        "coins": str(coins) + str(ctex),
+        "coins": str(coins['coins']) + str(ctex),
         "adm": 'False' 
          
     }
