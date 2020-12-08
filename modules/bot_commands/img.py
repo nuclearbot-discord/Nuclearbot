957e95eda68ab6ee9810c9b615ba9b9004efbb5e
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-08 22:28:34 +0300

    Хаг

--- modules/bot_commands/img.py
+++ modules/bot_commands/img.py
@@ -31,11 +31,11 @@ async def kiss (message, bot):
 @add_command ('yiff')
 async def yiff (message, bot):
     if message.channel.is_nsfw():
-        response = get ('https://api.furry.bot/V2/furry/yiff/gay')
+        response = get ('https://api.furry.bot/V2/furry/hug')
         json_data = json.loads (response.text)
         fs=json_data['images']
     
-        embed = discord.Embed (color = 0xff9900, title = 'йифф')
+        embed = discord.Embed (color = 0xff9900, title = 'это хаг')
         embed.set_image (url = str(fs[0]['url']))
     
         await message.channel.send (embed = embed)    
