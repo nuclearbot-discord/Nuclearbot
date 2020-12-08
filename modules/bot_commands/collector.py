55887af7f59ee32995f52232031c71b150d56176
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-08 19:40:08 +0300

    Update collector.py

--- modules/bot_commands/collector.py
+++ modules/bot_commands/collector.py
@@ -1,7 +1,7 @@
 from modules.bot_commands.economy import *
 from modules.bot_commands.img import *
 from modules.bot_commands.easter_eggs_module import *
-from modules.bot_commands.chat import *
+
 
 #reqs = ['sleep', 'Random', 'settings']
 #__all__ = ['commands_dict', 'egg_dict', 'chat_bot', 'settings', *reqs]
