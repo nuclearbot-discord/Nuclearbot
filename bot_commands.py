8406ca54596d91ac895d2411d6b67dbddec63cbe
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-08 16:46:55 +0300

    Update bot_commands.py

--- bot_commands.py
+++ bot_commands.py
@@ -103,7 +103,7 @@ async def kiss (message, bot):
     json_data = json.loads (response.text)
         
     
-    embed = discord.Embed (color = 0xff9900, title = f'{message.author} kiss {args[0]}. ')
+    embed = discord.Embed (color = 0xff9900, title = f'{message.author.mention} kiss {args[0]}. ')
     embed.set_image (url = str(json_data["url"]))
     await message.channel.send(args)
     await message.channel.send (embed = embed)  
