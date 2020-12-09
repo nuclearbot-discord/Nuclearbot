cc0aed0224c706c8bce77df934c381255de612bb
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-09 11:34:57 +0300

    Update stat.py

--- modules/bot_commands/stat.py
+++ modules/bot_commands/stat.py
@@ -13,19 +13,17 @@ async def minecraft (message, bot):
     args = get_next (message, 'minecraft').split (' ')
     nickarg=args
     if stat(nickarg)["status"] == "ok":
-    datamc = stat(nickarg)["data"]
-    if datamc["online"]=="1":
-        onl="online"
-    else:
-        onl = "offline"
-    if datamc["uuid"] !="False":
-        uuid="не найдено"
-    else:
-        uuid=datamc["uuid"]
-    ts = int(datamc["last_play"])
+        datamc = stat(nickarg)["data"]
+        if datamc["online"]=="1":
+            onl="online"
+        else:
+            onl = "offline"
+        if datamc["uuid"] !="False":
+            uuid="не найдено"
+        else:
+            uuid=datamc["uuid"]
+        ts = int(datamc["last_play"])
     if stat(nickarg)["status"]=="ok":
-    # if you encounter a "year is out of range" error the timestamp
-    # may be in milliseconds, try `ts /= 1000` in that case
         lps=datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
         message.channel.send(f'nick: {datamc["name"]}\nвсего играл (часов): {datamc["total_time_play"]}\nонлайн: {onl}\nпоследняя игра: {lps}\nuuid: {uuid}')
     else:
