b4d5cc9178fe2901d1b2316fdc2ea636d5396b61
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-09 12:44:55 +0300

    Update template_module.py

--- modules/bot_commands/template_module.py
+++ modules/bot_commands/template_module.py
@@ -3,4 +3,4 @@ from modules.bot_commands.for_commands import *
 __ver__ = 'X.X'
 
 print (f': {__name__}.py {__ver__}')
-await bot.get_channel (settings ['logs']).send (f'file ready: {__name__}.py {__ver__}')
+
