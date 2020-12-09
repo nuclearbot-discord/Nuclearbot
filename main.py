4d5bf16379d37698c1c0cfd38d8602bebaf4fc5d
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-09 13:45:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -112,6 +112,6 @@ async def on_message (message):
             pass
         
         return
-def versiton(vers):
+async def versiton(vers):
     await bot.get_channel (settings ['logs']).send(f'версия {vers},файл main.)
 bot.run (TOKEN)
