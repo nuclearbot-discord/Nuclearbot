a020398eb2e40cecfcb243e3a36ae7c173606144
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-09 12:12:26 +0300

    Update easter_eggs_module.py

--- modules/bot_commands/easter_eggs_module.py
+++ modules/bot_commands/easter_eggs_module.py
@@ -45,4 +45,4 @@ async def logger (msg, bot):
 __ver__ = '0.4'
 
 print (f': {__name__}.py {__ver__}')
-await bot.get_channel (f'file ready: {__name__}.py {__ver__}')
+await bot.get_channel (settings ['logs']).send (f'file ready: {__name__}.py {__ver__}')
