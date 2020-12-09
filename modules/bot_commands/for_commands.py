af215a6f49a37ec862471eefc591f43747b6815b
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-09 11:24:53 +0300

    Update for_commands.py

--- modules/bot_commands/for_commands.py
+++ modules/bot_commands/for_commands.py
@@ -3,6 +3,7 @@ import random
 from random import Random
 from asyncio import sleep
 from datetime import date
+from datetime import datetime
 import asyncio 
 import discord
 from discord.ext import commands
