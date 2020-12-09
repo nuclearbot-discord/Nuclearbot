3d4751e800336a2444b173b0c82859d8e408e426
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-09 13:12:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ async def on_ready ():
 
     await bot.get_channel (settings ['channel']).send (txt_bot_online.format (ver))
     await bot.get_channel (settings ['logs']).send (f'{logson} пинг: {bot.latency}')
-    version(ver)
+    versiton(ver)
     await bot.change_presence (
         status = discord.Status.idle
     )
@@ -112,6 +112,6 @@ async def on_message (message):
             pass
         
         return
-def version(ver):
-       await bot.get_channel (settings ['logs']).send(f'версия {ver},файл main.)
+def versiton(ver):
+    await bot.get_channel (settings ['logs']).send(f'версия {ver},файл main.)
 bot.run (TOKEN)
