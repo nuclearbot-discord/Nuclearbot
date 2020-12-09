195079ddb0e9d3cb367b048b8ce4053e15c86115
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-09 12:09:41 +0300

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -12,4 +12,4 @@ settings = {
 __ver__ = '4'
 
 print (f': {__name__}.py {__ver__}')
-await bot.get_channel (f'file ready: {__name__}.py {__ver__}')
+await bot.get_channel (settings ['logs']).send (f'file ready: {__name__}.py {__ver__}')
