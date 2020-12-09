dfed9125088cd6580a330e34ebc95e33c98ebeb9
Misha Shlykov <60094849+Misha-python@users.noreply.github.com>
2020-12-09 12:00:41 +0300

    Update stat.py

--- modules/bot_commands/stat.py
+++ modules/bot_commands/stat.py
@@ -1,32 +1,42 @@
-
 from modules.bot_commands.for_commands import *
 
 __ver__ = '1.0'
 
-#print (f': {__name__}.py {__ver__}')
 def stat (nick):
-    response = get(f'https://minecraft-statistic.net/api/player/info/{nick}/')
-    json_data = json.loads(response.text)
+    response = get (f'https://minecraft-statistic.net/api/player/info/{nick}/')
+    json_data = json.loads (response.text)
     return json_data
+
 @add_command('minecraft')
 async def minecraft (message, bot):
-    args = get_next (message, 'minecraft').split (' ')
-    nickarg="blockmambus"
-    if stat(nickarg)["status"] == "ok":
+    args = get_next (message, 'minecraft')
+    nickarg=args
+    
+    if stat (nickarg) ["status"] == "ok":
         datamc = stat(nickarg)["data"]
+        
         if datamc["online"]=="1":
-            onl="online"
+            onl = "online"
+            
         else:
             onl = "offline"
+            
         if datamc["uuid"] !="False":
-            uuid="не найдено"
+            uuid = "не найдено"
+            
         else:
             uuid=datamc["uuid"]
+            
         ts = int(datamc["last_play"])
-
-    # if you encounter a "year is out of range" error the timestamp
-        if stat(nickarg)["status"]=="ok":   # may be in milliseconds, try `ts /= 1000` in that case
+        
+        if stat(nickarg)["status"]=="ok":
+        # if you encounter a "year is out of range" error the timestamp
+        # may be in milliseconds, try `ts /= 1000` in that case
             lps=datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
             await message.channel.send(f'nick: {datamc["name"]}\nвсего играл (часов): {datamc["total_time_play"]}\nонлайн: {onl}\nпоследняя игра: {lps}\nuuid: {uuid}')
-     else:
-         await message.channel.send(stat(nickarg)['msg'])
+
+    else:
+        await message.channel.send(stat(nickarg)['msg'])
+
+print (f': {__name__}.py {__ver__}')
+
