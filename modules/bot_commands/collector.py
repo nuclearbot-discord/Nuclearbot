06f4a9aab951d5ec280d32dc1dd227b351c72034
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-09 14:13:59 +0300

    Update collector.py

--- modules/bot_commands/collector.py
+++ modules/bot_commands/collector.py
@@ -3,7 +3,7 @@ from modules.bot_commands.img import *
 from modules.bot_commands.easter_eggs_module import *
 from modules.bot_commands.unsorted import *
 from modules.bot_commands.stat import *
-from modules.bot_commands.for_commands import *
+
 __ver__ = '0.3'
 
 print (f': {__name__}.py {__ver__}')
