623f2988a7936e6fdc7dc603b2c3da3f03f106f9
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-09 21:14:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def on_ready ():
     await bot.get_channel (settings ['channel']).send (txt_bot_online.format (ver))
     await bot.get_channel (settings ['logs']).send (f'{logson} пинг: {bot.latency}')
     sps=statusnik
-    await bot.get_channel (settings ['logs']).send (*sps)
+    await bot.get_channel (settings ['logs']).send (sps[0])
     await bot.change_presence (
         status = discord.Status.idle
     )
